4086405bf2a2cf8484b379037f5ac43758317b77
Sito8943 <sito8943@gmail.com>
2022-09-17 07:49:00 -0400

    no fetch

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-const dotenv = require("dotenv");
-const axios = require("axios");
-
-dotenv.config();
-const {CURRENCY_API_LAYER} = process.env;
-
-/* services */
-
-
-const fetchCurrencies = async () => {
-    try {
-        const currencies = await axios.get('https://api.apilayer.com/currency_data/live?base=USD&symbols=EUR,GBP', {headers: {apiKey: CURRENCY_API_LAYER}});
-        console.log(currencies);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-
 /* logs */
 const showWelcomeMessage = () => console.log("Welcome to Currency Converter");
 
 
 showWelcomeMessage();
-fetchCurrencies();
+showCurrencies();
